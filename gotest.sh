13dec62b88c8e008ba99a8708351ba7aa2dadee1
kitung <kitung@ualberta.ca>
2016-12-08 15:59:33 -0700

    Add files via upload

--- gotest.sh
+++ gotest.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-# Scripts to run NICER code tests -- Sharon's computer settings
 times
-base="/Users/sharon/code/Albert"
-exe_dir="$base/Spot-master-11"
+base="/Users/kitung/Desktop/thesis_material"
+exe_dir="$base/Spot"
 #pwd
 make spot
 times
-out_dir="$exe_dir/OBLATE-gamma"
+out_dir="$exe_dir/bb_3_beaming"
 
 
 normalizeFlux=false  # false = no, true = yes; still need to add or remove -N flag in command line (-N means yes)
 
 # integers
-numtheta=1     # number of theta bins; for a small spot, only need one
-NS_model=1     # 1 (oblate) or 3 (spherical); don't use 2
-numbins=128    # phase bins
-numbands=1     # energy bands
-spectraltype=0 # Blackbody observed at one Energy
-elow=0.3
-ehi=0.8
+numtheta=5     # number of theta bins; for a small spot, only need one
+NS_model=2     # 1 (oblate 2014 A&M), 2 (oblate 2007 MLCB), or 3 (spherical);
+numbins=16    # phase bins
+numbands=5     # energy bands
+spectraltype=2 # Integrated Blackbody
+elow=0.3	   # Lowest energy boundary
+ehi=1.8		   # Highest energy boundary
 beaming=0      # isotropic emission, no beaming
-spotmodel=0
+spotmodel=0	   # circular spot
 
 
 # doubles -- using "e" notation
@@ -32,9 +31,9 @@ inclination=90 # in degrees
 emission=90 # in degrees
 rho=1.0  # in radians
 phaseshift=0.0 # this is used when comparing with data
-temp=0.1 # in keV (Surface temperature)
-#distance=3.08567758e20 # 10 kpc in meters
-distance=6.1713552e18 # 200 pc in meters
+temp=2 # in keV (Surface temperature)
+distance=3.08567758e20 # 10 kpc in meters
+#distance=6.1713552e18 # 200 pc in meters
 
 
 ## MAKING THE DATA FILE
@@ -42,15 +41,55 @@ if test ! -d "$out_dir"
    	then mkdir -p "$out_dir"
 fi
 
-# TEST OS1a = 600 Hz, tiny spot; oblate
+# TEST 1
 numtheta=5 # number of theta bins; for a small spot, only need one
 spotmodel=0
 NS_model=3
 beaming=0
 spectraltype=2
-out_file="$out_dir/test-5.txt"
+out_file="$out_dir/bb_only.txt"
 ## RUNNING THE CODE
 ./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt"
 times
 
+# Test 1a
+out_file="$out_dir/bb_only_2.txt"
+## RUNNING THE CODE
+./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt" -2
+times
+
+# TEST 2
+beaming=1
+out_file="$out_dir/bb_chan.txt"
+## RUNNING THE CODE
+./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt"
+times
+
+# TEST 2a
+out_file="$out_dir/bb_chan_2.txt"
+## RUNNING THE CODE
+./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt" -2
+times
+
+# TEST 3
+beaming=7
+out_file="$out_dir/bb_hopf.txt"
+## RUNNING THE CODE
+./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt"
+times
+
+# TEST 3a
+out_file="$out_dir/bb_hopf_2.txt"
+## RUNNING THE CODE
+./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt" -2
+times
+
+# TEST 4
+out_file="$out_dir/hyd.txt"
+spectraltype=3
+beaming=3
+temp=0.1
+## RUNNING THE CODE
+./spot -m "$mass" -r "$radius" -f "$spin" -i "$inclination" -e "$emission" -l "$phaseshift" -n "$numbins" -q "$NS_model" -o "$out_file" -p "$rho" -T "$temp" -D "$distance" -t "$numtheta" -g "$beaming" -s "$spectraltype" -S "$numbands" -u "$elow" -U "$ehi" -P "$spotmodel" -b "angles100.txt" -2
+times
 
