556482ada896d9c797e0d7180ac957e03022e595
kitung <kitung@ualberta.ca>
2017-05-07 18:19:54 +0000

    Add files via upload

--- fitness.m
+++ fitness.m
@@ -27,46 +27,48 @@ for i=size(X,2):-1:1  % we want to count backwards here
         timeShift=X(5,i);        
         rho=X(6,i);
         temperature=X(7,i);
+        distance=X(8,i);
        
         
-        % Initialize background to zero
-        %background1=extPar.fixed.background(1);
-        %background2=extPar.fixed.background(2);
-        %background3=extPar.fixed.background(3);
-        %background4=extPar.fixed.background(4);
-        %background5=extPar.fixed.background(5);
-        %background6=extPar.fixed.background(6);
-        %background7=extPar.fixed.background(7);
-        %background8=extPar.fixed.background(8);
-        %background9=extPar.fixed.background(9);
-        %background10=extPar.fixed.background(10);
-        %background11=extPar.fixed.background(11);
-        %background12=extPar.fixed.background(12);
-        %background13=extPar.fixed.background(13);
-        %background14=extPar.fixed.background(14);
-        %background15=extPar.fixed.background(15);
-      
+        % Initialize background
+        %{
+        background1=extPar.fixed.background(1);
+        background2=extPar.fixed.background(2);
+        background3=extPar.fixed.background(3);
+        background4=extPar.fixed.background(4);
+        background5=extPar.fixed.background(5);
+        background6=extPar.fixed.background(6);
+        background7=extPar.fixed.background(7);
+        background8=extPar.fixed.background(8);
+        background9=extPar.fixed.background(9);
+        background10=extPar.fixed.background(10);
+        background11=extPar.fixed.background(11);
+        background12=extPar.fixed.background(12);
+        background13=extPar.fixed.background(13);
+        background14=extPar.fixed.background(14);
+        background15=extPar.fixed.background(15);
+        %}
         
-        background1=X(8,i);
-        background2=X(9,i);
-        background3=X(10,i);
-        background4=X(11,i);
-        background5=X(12,i);
-        background6=X(13,i);
-       	background7=X(14,i);
-        background8=X(15,i);
-        background9=X(16,i);
-        background10=X(17,i);
-        background11=X(18,i);
-        background12=X(19,i);
-        background13=X(20,i);
-        background14=X(21,i);
-        background15=X(22,i);
-       
+        background1=X(9,i);
+        background2=X(10,i);
+        background3=X(11,i);
+        background4=X(12,i);
+        background5=X(13,i);
+        background6=X(14,i);
+       	background7=X(15,i);
+        background8=X(16,i);
+        background9=X(17,i);
+        background10=X(18,i);
+        background11=X(19,i);
+        background12=X(20,i);
+        background13=X(21,i);
+        background14=X(22,i);
+        background15=X(23,i);
+        
      
-
-        
-        cmd = '[Fspot(i),auxOutput{i}] = spotMex_trial(mass, radius, extPar.fixed.freq, inclination, emission, timeShift, extPar.fixed.numbins, extPar.fixed.modelchoice, rho, temperature, extPar.fixed.distance, extPar.fixed.numtheta, extPar.fixed.spectral_model, extPar.fixed.numbands, extPar.fixed.E_band_lower_1, extPar.fixed.E_band_upper_1, extPar.fixed.beaming_model, extPar.fixed.spots_2, extPar.obsdata2.t, extPar.fixed.bend_file_is, extPar.fixed.mr, extPar.fixed.b, extPar.fixed.psi, extPar.fixed.dcosa, extPar.fixed.toa, extPar.fixed.spotshape, ObsTime, extPar.fixed.inst_curve, extPar.fixed.attenuation';
+        disp(background1)
+       
+        cmd = '[Fspot(i),auxOutput{i}] = spotMex_trial(mass, radius, extPar.fixed.freq, inclination, emission, timeShift, extPar.fixed.numbins, extPar.fixed.modelchoice, rho, temperature, extPar.fixed.distance, extPar.fixed.numtheta, extPar.fixed.spectral_model, extPar.fixed.numbands, extPar.fixed.E_band_lower_1, extPar.fixed.E_band_upper_1, extPar.fixed.beaming_model, extPar.fixed.spots_2, extPar.obsdata2.t, extPar.fixed.bend_file_is, extPar.fixed.mr, extPar.fixed.b, extPar.fixed.psi, extPar.fixed.dcosa, extPar.fixed.toa, extPar.fixed.spotshape, ObsTime, extPar.fixed.inst_curve, extPar.fixed.attenuation,extPar.fixed.inte';
         for j = 1:extPar.fixed.numbands
             cmd = [cmd,', extPar.obsdata2.f(',num2str(j),',:), extPar.obsdata2.err(',num2str(j),',:), background',num2str(j),''];
         end
