014f5f96f0afd65a5e5788edfcf9443079228426
smorsink <morsink@ualberta.ca>
2017-07-20 10:01:52 -0600

    Updated McPhac files

--- McPhac.cpp
+++ McPhac.cpp
@@ -29,87 +29,70 @@
 #include <stdio.h>
 using namespace std;
 
+int th_index(double cos_theta, class LightCurve* mexmcc){
+
+  int n_mu, i_mu;
+
+    //Find proper mu choice
+    n_mu = 1;
+    while (cos_theta > mexmcc->mccangl[n_mu] && n_mu < 50){
+    	n_mu += 1;
+    }
+    i_mu = n_mu - 1;
+    // n_mu += -1;
+
+    int ii_mu(i_mu-1);
+    if (ii_mu < 0)
+      ii_mu = 0;
+    if (ii_mu > 46)
+      ii_mu = 46;
 
 
+    return ii_mu;
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
 // Calculate the final interpolated intensity
 // This "new" version takes into account that the energy is really the ratio: E/kT
-double McPHACC3new(double E, double cos_theta, double T, double lgrav, class LightCurve mexmcc){
+double McPHACC3new(double E, double cos_theta, int theta_index, double T, double lgrav, double gvec[4], class LightCurve* curve){
+
+
+  class LightCurve mexmcc;
+  mexmcc = (*curve);
+
 	double lt, ener_index;
 	//double e0, e1, th0, th1, t0, t1;
-	double t0;
-	double I_int[9], J[5], K[3], L(0.0);
-	int i_f, i_lt, i_lgrav, i_mu, n_mu, first_inte;
-	//double mu0, mu1;
+	
+	double I_int[4], J[4], K[4], L(0.0);
+	int i_f, i_lt, i_lgrav, first_inte;
 	std::ofstream out1, out2;      // output stream; printing information to the output file
   
-       
-	
-	out2.open("InterpolationTests/interp.txt", std::ios_base::app);
-	out2 << "#log(E/T) E (keV) I/T^3       I       T (keV)  logT   logg   cosa   Interp  Error(I/T^3)" << std::endl;
-
+      	int ii_mu = theta_index;
 
 
     lt = log10(1E3 * (T * Units::EV / Units::K_BOLTZ));
-
-    // This part is purely for testing against the old mcphac file.
-    /*    
-    lt = 5.8;
-    T = pow(10,lt) * 1e-3/(Units::EV / Units::K_BOLTZ);
-    lgrav = 14.1;
-    */
-
-    //    std::cout << "McPhac logt = " << lt << std::endl;
 
     // Find the correct temperature range
     i_lt = (lt-5.1)/0.05 ; //if we need to load 1st temperature, i_lt = 0. this is discrete math
     if (i_lt < 1) i_lt = 1;
     if (i_lt > 26) i_lt = 26;
 
-    // std::cout << "log(T) = " << lt << std::endl;
-    //std::cout << "i_lt = " << i_lt << std::endl;
 
     i_lgrav = (lgrav-13.7)/0.1;
     if (i_lgrav < 1) i_lgrav = 1;
 
-    //Find proper mu choice
-    n_mu = 1;
-    while (cos_theta > mexmcc.mccangl[n_mu] && n_mu < 50){
-    	n_mu += 1;
-    }
-    i_mu = n_mu - 1;
-
-    //    mu0 = mexmcc.mccangl[i_mu+1];
-    //mu1 = mexmcc.mccangl[n_mu+1];
-
-
+ 
 
     // Now do a npt interpolation
-    double evec[7];
-    double ivec[7][7][7][7];
+    //double evec[5];
+    double ivec[4][4][4][4];
     double err, err1;
-    double muvec[7];
-    double gvec[7];
-    double tvec[7];
+    double tvec[4];
 
-    // for (int npt(2);npt<7;npt++){
     
-    int npt(4);
+    int npt(3);
     
     //Find proper freqency choice
     ener_index = (log10(E/T) + 1.30369)/0.0338;
@@ -118,170 +101,49 @@ double McPHACC3new(double E, double cos_theta, double T, double lgrav, class Lig
     if (i_f < 1) i_f = 1;
     if (i_f > 95) i_f=95;
 
-    int ii_mu(i_mu-1);
-    if (ii_mu < 0)
-      ii_mu = 0;
-    if (ii_mu > 46)
-      ii_mu = 46;
+    // int ii_mu = th_index( cos_theta, &mexmcc);
 
-    for (int r(0); r<4; r++){
+    for (int r(0); r<3; r++){
       tvec[r+1] =  5.1+0.05*(i_lt-1+r);
-      t0 = tvec[r+1];
+      //t0 = tvec[r+1];
       //std::cout << "tvec[r]=" << tvec[r+1] << std::endl;
-      for (int q(0); q<4; q++){
-	gvec[q+1] = 13.7+0.1*(i_lgrav-1+q);
+      for (int q(0); q<3; q++){
+	//gvec[q+1] = 13.7+0.1*(i_lgrav-1+q);
 	//std::cout << "logg = " << gvec[q+1] << std::endl;
-	for (int k(0); k<4; k++){
-	  muvec[k+1] =  mexmcc.mccangl[ii_mu+k];
-	  //muvec[k+1] =  acos(mexmcc.mccangl[ii_mu+k]);
-	  //std::cout << "muvec[k] = " << muvec[k+1] << std::endl;
-	  // Interpolate over Energy for fixed Teff, gravity, and mu
-	  //std::cout << "E = " << E << " T = " << T << " E/T =" << E/T << std::endl;  
-	  //std::cout << "Log10(E/T) = " << log10(E/T) << std::endl;
-
-	  /* if ( fabs(tvec[r+1] - 5.8) < 1e-10 && (gvec[q+1]==14.1) && (muvec[k+1]==.2323)){
-
-	    out1.open("InterpolationTests/LowT.txt", std::ios_base::trunc);
-
-	    for (int i(0); i<100; i++){
-
-	      
-	      double log_e_t = mexmcc.mcloget[1-1+i];
-	      double energy = pow(10,log_e_t) * pow(10,tvec[r+1]) * 1e-3/(Units::EV / Units::K_BOLTZ);
-	      first_inte = ((i_lt-1+r)*11 + i_lgrav-1+q) * 5000 + (1-1+i) * 50 + (ii_mu + k) +1 -1;
-	      double iovert3 = mexmcc.mccinte[first_inte];
-
-	      out1 << "#log(E/T) E (keV)    I/T^3        I    T (keV)  logT   logg   cosa index" << std::endl;
-	      out1 << log_e_t << "  " 
-			<< energy << " "
-			<< iovert3 << " "
-			<< iovert3 * pow(10.0,t0*3.0) << " "
-			<<  pow(10,tvec[r+1]) * 1e-3/(Units::EV / Units::K_BOLTZ) << " "
-			<< tvec[r+1] << "    "
-			<< gvec[q+1] << "   "
-			<< muvec[k+1] << " "
-			<< i
-			<< std::endl;
-
-
-	    }
-	    out1.close();*/
-	    
-	
-
-
+	for (int k(0); k<3; k++){
+	  //muvec[k+1] =  mexmcc.mccangl[ii_mu+k];
 	  for( int j(0); j<npt; j++){
 	    //evec[j] = pow(10,mexmcc.mcloget[i_f-1+j]);
-	    evec[j+1] = mexmcc.mcloget[i_f-1+j];
-	    first_inte = ((i_lt-1+r)*11 + i_lgrav-1+q) * 5000 + (i_f-1+j) * 50 + (ii_mu + k) +1 -1;
+	    //evec[j+1] = mexmcc.mcloget[i_f-1+j];
+	    first_inte = ((i_lt-1+r)*11 + i_lgrav-1+q) * 5000 + (i_f-1+j) * 50 + (ii_mu + k);
 
-	    //  ivec[r][q][k][j+1] = mexmcc.mccinte[first_inte]*pow(10.0,t0*3.0);
-
+	    // linear interpolation
 	    ivec[r][q][k][j+1] = mexmcc.mccinte[first_inte];
 
-	    //ivec[r][q][k][j+1] = log10(mexmcc.mccinte[first_inte]);
-
-	    /* std::cout << "j = " << j<< std::endl;
-	    std::cout << "E = " << E << " T = " << T << " E/T =" << E/T << std::endl;  
-	    std::cout << "Log10(E/T) = " << log10(E/T) << std::endl;
-	    std::cout << "evec[j] = " << evec[j+1] 
-	    	      << " ivec[j] = " << ivec[r][q][k][j+1]
-		      << " mcint = " << mexmcc.mccinte[first_inte]
-	    	      << std::endl;*/
-	    
+	    // logarithmic interpolation
+	    //ivec[r][q][k][j+1] = log10(mexmcc.mccinte[first_inte]);	    
 	  }
 	  
-	  //	  I_int[k+1] = polint(evec,ivec[r][q][k],npt,log10(E/T),&err1)*pow(10.0,t0*3.0);
-	  I_int[k+1] = polint(evec,ivec[r][q][k],npt,log10(E/T),&err1);
-	
-	  /* if ( fabs(tvec[r+1] - 6.4) < 1e-10 && (gvec[q+1]==14.1) && (muvec[k+1]==.2323)){
-
-	     I_int[k+1] = polint(evec,ivec[r][q][k],npt,log10(E/T),&err1)*pow(10.0,t0*3.0);
-	    //I_int[k+1] = printpolint(evec,ivec[r][q][k],npt,log10(E/T),&err1);
-
-	    // out2 << "#log(E/T) E (keV) I/T^3       I       T (keV)  logT   logg   cosa   Interp  Error(I/T^3)" << std::endl;
-	    
-	    out2 
-		    << log10(E/T) << " "
-		    << E << "   "
-		    << polint(evec,ivec[r][q][k],npt,log10(E/T),&err1) << "  "
-		    << I_int[k+1] << " "
-		    << T << " "
-		    << tvec[r+1] << "    "
-		    << gvec[q+1] << "   "
-		    << muvec[k+1] << " "
-		    << npt << "       " 
-		    << err1
-		    << endl;
-	  }
-	  else
-	    I_int[k+1] = polint(evec,ivec[r][q][k],npt,log10(E/T),&err1)*pow(10.0,t0*3.0);*/
-
-	  //if (isnan(I_int[k])){
-	  /* 
-	    cout << evec[1] << " " << evec[2] << " " << evec[3] << " " << evec[4] << " " << log10(E/T) << " " << ivec[r][q][k][1] << " " << ivec[r][q][k][2] << " " << ivec[r][q][k][3] << " " << ivec[r][q][k][4] << endl;
-	  	I_int[k] = printpolint(evec,ivec[r][q][k],4,log10(E/T),&err);
-	  	cout << "Interpolated Intensity = " << I_int[k]
-	  	  << "err = " << err << endl;
-	  */
-		//}
-	  /*  cout << "0 mu[k] = " << muvec[k+1]
-	     <<" E=" << E/T << " New 4pt Interpolated: I = " << I_int[k+1] 
-	     << " err = " << err << endl;*/
+	  // linear interpolation
+	  I_int[k+1] = polint(&mexmcc.mcloget[i_f-2],ivec[r][q][k],npt,log10(E/T),&err1);
+	 
+	  // logarithmic interpolation
+	  //I_int[k+1] = pow(10.0,polint(evec,ivec[r][q][k],npt,log10(E/T),&err1));	
 	}
 	// Intepolate over mu for fixed Teff, gravity
-	J[q+1] = polint(muvec,I_int,4,cos_theta,&err);
-	/*	cout 
-		  << " logg = " << gvec[q+1]
-		  << " costheta = " << cos_theta << " Interpolated I = " << J[q+1]
-		  << " err = " << err << std::endl;*/
+	J[q+1] = polint(&mexmcc.mccangl[ii_mu-1],I_int,3,cos_theta,&err);
       }
       // Interpolate over logg for fixed Teff
-      K[r+1] = polint(gvec,J,4,lgrav,&err);
-      /*cout << " logg = " << lgrav 
-         << " Interpolated I = " 
-         <<  K[r+1] 
-         << " err = " << err << endl;   */   
+      K[r+1] = polint(gvec,J,3,lgrav,&err);   
     }
 
-    L = polint(tvec,K,4,lt,&err)*pow(10.0,lt*3.0);
-    // L = polint(tvec,K,4,lt,&err);
+    // Interpolate over log(Teff)
+    L = polint(tvec,K,3,lt,&err)*pow(10.0,lt*3.0);
 
-
-    
-	    out2 
-		    << log10(E/T) << " "
-		    << E << "   "
-		    << L * pow(10.0,-lt*3.0) << "  "
-		    << L << " "
-		    << T << " "
-		    << lt << "    "
-		    << lgrav << "   "
-		    << cos_theta << " "
-		    << npt << "       " 
-		    << err1
-		    << endl;
-
-
-
-    
-    
-    /*    std::cout << " log(T_eff) = " << lt 
-    	      << " Interpolated I = " << L
-    	      << " err = " << err << std::endl;
-    */
-
-  
-    //  cout << L << endl;
     if (cos_theta < 0.015629) L = 0;
     
-    if (isnan(L)) {
-    	cout << cos_theta << " " << mexmcc.mccangl[i_mu] << " " << mexmcc.mccangl[n_mu] << endl;
-    	cout << I_int[0] << " " << J[0] << " " << J[1] << " " << J[2] << " " << J[3] << endl;
-    }
-
-        return L;
-    }
+    return L;
+}
 
 
 
@@ -298,7 +160,7 @@ double AtmosEBandFlux3new( unsigned int model, double cos_theta, double T, doubl
     int n_steps(0);             // number of energy points within band
     double flux(0.0);           // total integrated flux
 
- 
+    double gvec[4];
 
 
     double ener_spacing = pow(10.0,0.0338);
@@ -310,12 +172,13 @@ double AtmosEBandFlux3new( unsigned int model, double cos_theta, double T, doubl
 
     n_steps = 1* (e2_dex - e1_dex);
 
+    int theta_index = th_index(cos_theta,&mexmcc);
     // n_steps = 4;
 
 
     if (n_steps == 0){ // zero energy points within bandwidth: (4.1.3) one trapzoid
       //cout << "0 steps" << endl;
-      flux = (E2 - E1) / 2.0 * (McPHACC3new(E1,cos_theta, T, lgrav, mexmcc) / E1 + McPHACC3new(E2,cos_theta, T, lgrav, mexmcc) / E2);
+      flux = (E2 - E1) / 2.0 * (McPHACC3new(E1,cos_theta,theta_index, T, lgrav, gvec, &mexmcc) / E1 + McPHACC3new(E2,cos_theta,theta_index, T, lgrav, gvec, &mexmcc) / E2);
     }
     if (n_steps == 1){ // one energy points within bandwidth: (4.1.3) two trapzoids
       //cout << "1 step" << endl;
@@ -324,11 +187,12 @@ double AtmosEBandFlux3new( unsigned int model, double cos_theta, double T, doubl
 	
 	e_m = 0.5*(E2+E1);
 
-	double counts_m = McPHACC3new(e_m,cos_theta, T,lgrav, mexmcc) / e_m;
+	
+	double counts_m = McPHACC3new(e_m,cos_theta,theta_index, T,lgrav, gvec, &mexmcc) / e_m;
 
-        flux = (e_m - E1) / 2 * (McPHACC3new(E1,cos_theta, T, lgrav, mexmcc) / E1 
+        flux = (e_m - E1) / 2 * (McPHACC3new(E1,cos_theta,theta_index, T, lgrav, gvec, &mexmcc) / E1 
 				 + counts_m);  // first trapezoid
-        flux += (E2 - e_m) / 2 * (counts_m + McPHACC3new(E2,cos_theta, T,lgrav, mexmcc) / E2); // second trapezoid
+        flux += (E2 - e_m) / 2 * (counts_m + McPHACC3new(E2,cos_theta, theta_index, T,lgrav,gvec, &mexmcc) / E2); // second trapezoid
     }
     if (n_steps >= 2){ // two energy points within bandwidth: (4.1.3) three trapzoids
       //cout << "n steps= " << n_steps << endl;
@@ -337,19 +201,19 @@ double AtmosEBandFlux3new( unsigned int model, double cos_theta, double T, doubl
        double e_l;
        double counts_l;
 
-       flux = e_step * 0.5 * McPHACC3new(E1,cos_theta, T, lgrav, mexmcc) / E1;
+       flux = e_step * 0.5 * McPHACC3new(E1,cos_theta, theta_index, T, lgrav,gvec, &mexmcc) / E1;
        //cout << "flux = " << flux << endl;
 
        for (int i(1); i<=n_steps; i++){
 
 	 e_l = E1 + i*e_step;
-	 counts_l = McPHACC3new(e_l,cos_theta, T, lgrav, mexmcc) / e_l;
+	 counts_l = McPHACC3new(e_l,cos_theta,theta_index, T, lgrav,gvec, &mexmcc) / e_l;
 	 flux += e_step * (counts_l);
 	 //cout << "flux = " << flux << endl;
 
        }
 
-       flux += e_step * 0.5 *  McPHACC3new(E2,cos_theta, T,lgrav, mexmcc) / E2;
+       flux += e_step * 0.5 *  McPHACC3new(E2,cos_theta, theta_index, T,lgrav,gvec, &mexmcc) / E2;
        // cout << "flux = " << flux << endl;
 
     }
--- McPhac.h
+++ McPhac.h
@@ -1,8 +1,10 @@
 
 
-double McPHACC3new(double E, double cos_theta, double T, double lgrave, class LightCurve mexmcc);
+double McPHACC3new(double E, double cos_theta, int theta_index, double T, double lgrave, double ggvec[4], class LightCurve* mexmcc);
 
 
 
 double AtmosEBandFlux3new( unsigned int model, double cos_theta, double T, double lgrav, double E1, double E2, class LightCurve mexmcc);
 
+
+int th_index(double cos_theta, class LightCurve* mexmcc);
